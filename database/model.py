ed82dfd39848e265ae2eeee8aa120ca10d94ff9b
anoel <axou.noel@gmail.com>
2023-10-04 12:42:35 +0200

    début bdd

---
+++ database/model.py
@@ -0,0 +1,51 @@
+from database import db
+import enum
+from sqlalchemy import Enum
+
+class TypesMessages(enum.Enum):
+    string = "string"
+    file = "file"
+
+
+class Conversation(db.Model):
+    __tablename__ = 'conversation'
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
+    participants = db.Column(db.PickleType, index=True)
+
+    @classmethod
+    def add_conversation(cls, participants):
+        conv = Conversation()
+        conv.participants = participants
+        conv.save()
+
+
+class Message(db.Model):
+    __tablename__ = 'message'
+
+    id_conversation = db.Column(db.Integer, db.ForeignKey('conversation.id'), nullable=False, primary_key=True)
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    expediteur = db.Column(db.Integer, nullable=False)
+    type_message = db.Column(Enum(TypesMessages), nullable=False, default="string")
+    message = db.Column(db.String, nullable=False)
+
+    @classmethod
+    def add_message(cls, id_conversation, date, expediteur, type_message, message):
+        conv = Conversation.get(id_conversation)
+        if expediteur not in conv.participants:
+            return False, "erreur, expediteur non présent dans la conversation"
+        if type_message not in TypesMessages._member_map_.values():
+            return False, "Type de message non existant"
+        msg = Message()
+        msg.id_conversation = id_conversation
+        last_msg = Message.query.filter_by(id_conversation=id_conversation).order_by(Message.id.desc()).first()
+        print(last_msg)
+        msg.id = last_msg.id+1 if last_msg else 0
+        msg.date = date
+        msg.expediteur = expediteur
+        msg.type_message = type_message
+        msg.message = message
+        msg.save()
+        return True
+
--- main.py
+++ main.py
@@ -13,6 +13,14 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
 
+# ----------------------------------------
+
+from database import db
+from database.model import Conversation, Message, TypesMessages
+from datetime import datetime
+import random
+import string
+
 __prg_version__ = "0.0.1"
 __prg_name__ = "chat"
 
@@ -29,20 +37,33 @@ app.config['APP_NAME'] = "Chat :)"
 
 
 # Db sqlite
-#CHAT_DIR = os.path.dirname(os.path.abspath(__file__))
-#database_file = "sqlite:///{}".format(os.path.join(CHAT_DIR, "chat.db"))
-#app.config["SQLALCHEMY_DATABASE_URI"] = database_file
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+CHAT_DIR = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(CHAT_DIR, "chat.db"))
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 @app.route("/", methods=["GET", "POST"])
 def accueil_vendeur():
-    return render_template('home.html')
+    #conv = Conversation()
+    #conv.participants = [10,20]
+    # conv.save()
+    Message.add_message(1, datetime.now(), 10,
+                        TypesMessages.string, get_random_string(8))
+    return render_template('home.html', conversations=Conversation.all(), messages=Message.query.order_by(Message.id_conversation.asc()).all())
+
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
 
 
 def create_app():
-    # db.init_app(app)
-    # with app.app_context():
-    # db.create_all()
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     with app.app_context():
         for bp in app.blueprints:
             if 'init_db' in dir(app.blueprints[bp]):
